0e3f70fb528b3a695dc9480a424e6bd4c9cc8e40
Pablo Recio <pablo@recio.me>
2024-02-27 18:24:02 +0100

    Handle some exceptions from Mercadona receipts

--- invoice_parser/src/parser/mercadona.py
+++ invoice_parser/src/parser/mercadona.py
@@ -5,13 +5,16 @@ from typing import Any
 from src.parser.utils import combine_lines, extract_lines_from_pdf
 
 ITEM_REGEX = r"(\d+)(.*?)(\d+,\d{2})?(?:\s+(\d+,\d{2}))$"
-regex = re.compile(ITEM_REGEX)
+item_regex = re.compile(ITEM_REGEX)
+
+WEIGHTED_ITEM_REGEX = r"(.*)\s(\d,\d{3})\skg\s(\d,\d{2})\s€/kg$"
+weighted_item_regex = re.compile(WEIGHTED_ITEM_REGEX)
 
 
 class ReceiptCrawler:
     @classmethod
-    def __return_groups_or_none(cls, line):
-        match = regex.match(line)
+    def __return_groups_or_none(cls, line: str):
+        match = item_regex.match(line)
         if match:
             groups = match.groups()
 
@@ -20,14 +23,44 @@ class ReceiptCrawler:
             return False
 
     @classmethod
+    def __to_decimal(cls, number_str: str) -> Decimal:
+        return Decimal(number_str.replace(",", "."))
+
+    @classmethod
     def __groups_to_dict(cls, groups):
-        total = Decimal(groups[3].replace(",", "."))
-        return {
-            "amount": int(groups[0]),
-            "name": groups[1].strip(),
-            "cost_unit": Decimal(groups[2].replace(",", ".")) if groups[2] else total,
-            "cost_total": total,
-        }
+        total = cls.__to_decimal(groups[3])
+
+        item_name_match = weighted_item_regex.match(groups[1])
+        if item_name_match:
+            return {
+                "amount": cls.__to_decimal(item_name_match.group(2)),
+                "name": item_name_match.group(1),
+                "cost_unit": cls.__to_decimal(item_name_match.group(3)),
+                "cost_total": total,
+            }
+        else:
+            amount = groups[0]
+            name = groups[1]
+            cost_unit = cls.__to_decimal(groups[2]) if groups[2] else total
+
+            # Need to handle the case
+            #   16 HUEVOS CAMPEROS
+            # to turn into
+            #   6 HUEVOS CAMPEROS
+            units_len = len((total / cost_unit).to_eng_string())
+
+            overflow = groups[0][units_len:]
+
+            if overflow:
+                amount = groups[0][:units_len]
+                name = f"{overflow}{name}"
+
+            return {
+                "amount": int(amount),
+                "name": name.strip(),
+                "cost_unit": cost_unit,
+                "cost_total": total,
+            }
 
     @classmethod
     def extract_items(cls, path: str) -> list[dict[str, Any]]:
