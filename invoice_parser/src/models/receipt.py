0adb45d811ec752f982853cc4d3c2a5a4f504138
Pablo Recio <pablo@recio.me>
2024-03-11 13:11:31 +0100

    Model serializer to output the receipt's total

--- invoice_parser/src/models/receipt.py
+++ invoice_parser/src/models/receipt.py
@@ -1,6 +1,7 @@
 from datetime import datetime
+from typing import Any
 
-from pydantic import BaseModel
+from pydantic import BaseModel, model_serializer
 from src.models.item import Item
 
 
@@ -11,3 +12,11 @@ class Receipt(BaseModel):
     @property
     def total_amount(self):
         return sum([i.cost_total for i in self.items])
+
+    @model_serializer()
+    def serialize_model(self) -> dict[str, Any]:
+        return {
+            "datetime": self.datetime,
+            "items": self.items,
+            "total_amount": self.total_amount,
+        }
--- invoice_parser/tests/models/test_receipt.py
+++ invoice_parser/tests/models/test_receipt.py
@@ -1,5 +1,7 @@
+import json
 from datetime import datetime
 from decimal import Decimal
+from typing import Any
 
 import pytest
 from src.models.item import Item
@@ -43,3 +45,80 @@ from src.models.receipt import Receipt
 def test_total_amount(items: list[Item], expected: Decimal):
     receipt = Receipt(datetime=datetime.now(), items=items)
     assert receipt.total_amount == expected
+
+
+@pytest.mark.parametrize(
+    ("items", "dt", "expected"),
+    [
+        (
+            [],
+            datetime(2023, 12, 21),
+            {
+                "datetime": datetime(2023, 12, 21).strftime("%Y-%m-%dT%H:%M:%S"),
+                "items": [],
+                "total_amount": Decimal(0),
+            },
+        ),
+        (
+            [
+                Item(
+                    name="LECHE",
+                    amount=Decimal(1),
+                    cost_unit=Decimal("1.56"),
+                    cost_total=Decimal("1.56"),
+                ),
+            ],
+            datetime(2023, 12, 21),
+            {
+                "datetime": datetime(2023, 12, 21).strftime("%Y-%m-%dT%H:%M:%S"),
+                "items": [
+                    {
+                        "name": "LECHE",
+                        "amount": "1",
+                        "cost_unit": "1.56",
+                        "cost_total": "1.56",
+                    }
+                ],
+                "total_amount": "1.56",
+            },
+        ),
+        (
+            [
+                Item(
+                    name="LECHE",
+                    amount=Decimal(1),
+                    cost_unit=Decimal("1.56"),
+                    cost_total=Decimal("1.56"),
+                ),
+                Item(
+                    name="TORTITAS MAIZ",
+                    amount=Decimal(2),
+                    cost_unit=Decimal("1.99"),
+                    cost_total=Decimal("3.98"),
+                ),
+            ],
+            datetime(2023, 12, 21),
+            {
+                "datetime": datetime(2023, 12, 21).strftime("%Y-%m-%dT%H:%M:%S"),
+                "items": [
+                    {
+                        "name": "LECHE",
+                        "amount": "1",
+                        "cost_unit": "1.56",
+                        "cost_total": "1.56",
+                    },
+                    {
+                        "name": "TORTITAS MAIZ",
+                        "amount": "2",
+                        "cost_unit": "1.99",
+                        "cost_total": "3.98",
+                    },
+                ],
+                "total_amount": "5.54",
+            },
+        ),
+    ],
+)
+def test_model_serializer(items: list[Item], dt: datetime, expected: dict[str, Any]):
+    receipt = Receipt(datetime=dt, items=items)
+    assert json.loads(receipt.model_dump_json()) == expected
