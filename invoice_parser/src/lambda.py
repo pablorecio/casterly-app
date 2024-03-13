bb943f568a6652ef558016f47bd5fa47c57208ca
Pablo Recio <pablo@recio.me>
2024-03-13 19:59:40 +0100

    Initial terraform configuration for basic lambda function

---
+++ invoice_parser/src/lambda.py
@@ -0,0 +1,3 @@
+def lambda_handler(event, context):
+    message = "Hello {} !".format(event["key1"])
+    return {"message": message}
---
+++ terraform/main.tf
@@ -0,0 +1,84 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.40"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_s3_bucket" "user_receipts" {
+  bucket = "casterly-user-receipts"
+
+  tags = {
+    Name = "User Receipts"
+  }
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name               = "Spacelift_Test_Lambda_Function_Role"
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "lambda.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "iam_policy_for_lambda" {
+
+  name        = "aws_iam_policy_for_terraform_aws_lambda_role"
+  path        = "/"
+  description = "AWS IAM Policy for managing aws lambda role"
+  policy      = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": [
+       "logs:CreateLogGroup",
+       "logs:CreateLogStream",
+       "logs:PutLogEvents"
+     ],
+     "Resource": "arn:aws:logs:*:*:*",
+     "Effect": "Allow"
+   }
+ ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.iam_policy_for_lambda.arn
+}
+
+data "archive_file" "zip_the_python_code" {
+  type        = "zip"
+  source_dir  = "${path.module}/../invoice_parser/src/"
+  output_path = "${path.module}/../invoice_parser/src/invoice-parser.zip"
+}
+
+resource "aws_lambda_function" "terraform_lambda_func" {
+  filename      = "${path.module}/../invoice_parser/src/invoice-parser.zip"
+  function_name = "Invoice_Parser_Lambda_Function"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "lambda.lambda_handler"
+  runtime       = "python3.12"
+  depends_on    = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+}
