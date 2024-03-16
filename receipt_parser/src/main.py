5410863385697b5cc7a6dba1583f707f40c59cec
Pablo Recio <pablo@recio.me>
2024-03-16 20:23:50 +0100

    Lambda function writes into new S3 bucket

--- receipt_parser/src/main.py
+++ receipt_parser/src/main.py
@@ -1,4 +1,3 @@
-import json
 import logging
 import tempfile
 
@@ -11,18 +10,27 @@ logger = logging.getLogger()
 logger.setLevel("INFO")
 s3_client = boto3.client("s3")
 
+DL_BUCKET = "casterly-app-live-dl"
+
 
 def lambda_handler(event, context):
     s3_bucket_name = event["Records"][0]["s3"]["bucket"]["name"]
     s3_file_name = event["Records"][0]["s3"]["object"]["key"]
 
+    splitted_file_name = s3_file_name.split("/")
+    user_id = splitted_file_name[0]
+    store_name = splitted_file_name[1]
+
     parser_class = object
-    if s3_file_name.startswith("mercadona"):
+    if store_name == "mercadona":
         logger.info("Mercadona receipt")
         parser_class = MercadonaReceiptParser
-    elif s3_file_name.startswith("carrefour"):
+    elif store_name == "carrefour":
         logger.info("Carrefour receipt")
         parser_class = CarrefourReceiptParser
+    else:
+        logger.error("Unexpected store '%s'", store_name)
+        raise Exception(f"Unexpected store '{store_name}'")
 
     with tempfile.NamedTemporaryFile(mode="w+b") as f:
         file_path = f"s3://{s3_bucket_name}/{s3_file_name}"
@@ -35,6 +43,13 @@ def lambda_handler(event, context):
             raise
 
         receipt = parser_class(f.name).get_receipt_model()
-        logger.info(receipt.model_dump_json())
 
-    return json.loads(receipt.model_dump_json())
+    output_json = receipt.model_dump_json()
+    logger.info(output_json)
+    s3_client.put_object(
+        Bucket=DL_BUCKET,
+        Key=f"{user_id}/{store_name}/{receipt.datetime}.json",
+        Body=output_json,
+    )
+
+    return 200, {"result": "success"}
