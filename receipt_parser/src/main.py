7a3966fb317a01f38ae58ecc6294b693f19b9d6c
Pablo Recio <pablo@recio.me>
2024-03-16 11:16:09 +0100

    Uses logging instead of print

--- receipt_parser/src/main.py
+++ receipt_parser/src/main.py
@@ -1,9 +1,13 @@
+import json
+import logging
 import tempfile
 
 import boto3
 from src.parser.carrefour import ReceiptCrawler as CarrefourRC
 from src.parser.mercadona import ReceiptCrawler as MercadonaRC
 
+logger = logging.getLogger()
+logger.setLevel("INFO")
 s3_client = boto3.client("s3")
 
 
@@ -13,18 +17,16 @@ def lambda_handler(event, context):
 
     crawler_class = object
     if s3_file_name.startswith("mercadona"):
-        print("Mercadona receipt")
+        logger.info("Mercadona receipt")
         crawler_class = MercadonaRC
     elif s3_file_name.startswith("carrefour"):
-        print("Carrefour receipt")
+        logger.info("Carrefour receipt")
         crawler_class = CarrefourRC
 
     with tempfile.NamedTemporaryFile(mode="w+b") as f:
         s3_client.download_fileobj(s3_bucket_name, s3_file_name, f)
 
-        print(f.name)
-        print(f.name.__class__)
         receipt = crawler_class.extract_items(f.name)
-        print(receipt.model_dump_json())
+        logger.info(receipt.model_dump_json())
 
-    return {}
+    return json.loads(receipt.model_dump_json())
