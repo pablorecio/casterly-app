51c5882ba20dfbd36d0ccba14dd4faa687a648fb
Pablo Recio <pablo@recio.me>
2024-03-16 09:06:20 +0100

    Print statments for debugging

--- receipt_parser/src/main.py
+++ receipt_parser/src/main.py
@@ -13,13 +13,18 @@ def lambda_handler(event, context):
 
     crawler_class = object
     if s3_file_name.startswith("mercadona"):
+        print("Mercadona receipt")
         crawler_class = MercadonaRC
     elif s3_file_name.startswith("carrefour"):
+        print("Carrefour receipt")
         crawler_class = CarrefourRC
 
     with tempfile.TemporaryFile(mode="w+b") as f:
         s3_client.download_fileobj(s3_bucket_name, s3_file_name, f)
 
-        print(crawler_class.extract_items(f.name).model_dump_json())
+        print(f.name)
+        print(f.name.__class__)
+        receipt = crawler_class.extract_items(f.name)
+        print(receipt.model_dump_json())
 
     return {}
